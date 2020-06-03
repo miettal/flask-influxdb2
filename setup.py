4070668916f69b8e227ca659cdd83ab044c08d5f
Hiromasa Ihara <iharahiromasa@gmail.com>
2020-06-03 11:06:29 +0900

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-InfluxDB2',
-    version='0.1',
+    version='0.2',
     url='https://github.com/miettal/flask-influxdb2/',
     license='BSD',
     author='Hiromasa Ihara',
